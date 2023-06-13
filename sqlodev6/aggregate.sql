2b4c390b7cefafb41d04e104e5d8b57ed195a316
Onur Berk Gunes <obgunes99@gmail.com>
2023-06-13 15:26:27 +0300

    SQL HW6 Patika

---
+++ sqlodev6/aggregate.sql
@@ -0,0 +1,10 @@
+select round(avg(rental_rate), 2) from film;
+
+select count(*) from film
+where title like 'C%';
+
+select max(length) from film
+where rental_rate = 0.99;
+
+select count(distinct replacement_cost) from film
+where length > 150;
